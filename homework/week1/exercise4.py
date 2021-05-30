347889eda4f9406ffc15c14fd33f186d848cd427
bnowe88 <84686331+bnowe88@users.noreply.github.com>
2021-05-30 14:47:02 -0700

    WEEK 1 EXERCISES AND HOMEWORK

---
+++ homework/week1/exercise4.py
@@ -0,0 +1,11 @@
+"""
+push: updates the remote branch with local commits. 
+It is one of the four commands in Git that prompts interaction with the remote repository. 
+You can also think of git push as update or publish.
+commit: creates a commit, which is like a snapshot of your repository. 
+These commits are snapshots of your entire repository at specific times. 
+Commits are the building blocks of "save points" within Git's version control.
+pull: updates your current local working branch, and all of the remote tracking branches. 
+It's a good idea to run git pull regularly on the branches you are working on locally.
+Source - Git Guides
+"""
